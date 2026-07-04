71a213540a4717ad74911bef0aaef24087aaac03
vishnuvardhanrangasamy-ux <vishnuvardhanrangasamy@gmail.com>
2026-07-04 14:55:56 +0530

    Add files via upload
    Day 1

---
+++ GCD1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class GCD1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int gcd = 1;
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0)
+            {
+                gcd = i;
+            }
+        }
+        System.out.println(gcd);
+    }
+}
