23a7dbb2faddaffa6613bbf5df3878956aa52e42
vishnuvardhanrangasamy-ux <vishnuvardhanrangasamy@gmail.com>
2026-07-06 09:38:10 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class array3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+             if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
